f52200a2c67a1060253f4ecad03f2a708bd2edf3
Benjamin Hulsey <121725178+TardisMoon@users.noreply.github.com>
2024-04-15 11:01:31 -0500

    Update Mutex.cpp

--- Os/FreeRTOS/Mutex.cpp
+++ Os/FreeRTOS/Mutex.cpp
@@ -1,61 +1,33 @@
 #include "FreeRTOS.h"
-#include "task.h"
 #include "semphr.h"
 
-// Define Global
-SemaphoreHandle_t xMutex;
+class Mutex {
+private:
+    SemaphoreHandle_t mutex; // FreeRTOS mutex variable
 
-void Task1(void *pvParameters)
-{
-    while (1)
-    {
-        // Take Mutex
-        if (xSemaphoreTake(xMutex, portMAX_DELAY) == pdTRUE)
-        {
-           
-            // Release Mutex
-            xSemaphoreGive(xMutex);
-        }
-
-        // Delay if needed
-        vTaskDelay(pdMS_TO_TICKS(100));
-    }
-}
-
-void Task2(void *pvParameters)
-{
-    while (1)
-    {
-        // Take Mutex
-        if (xSemaphoreTake(xMutex, portMAX_DELAY) == pdTRUE)
-        {
-            
-
-            // Release Mutex
-            xSemaphoreGive(xMutex);
-        }
-
-        // Delay if needed
-        vTaskDelay(pdMS_TO_TICKS(100));
-    }
-}
-
-int main(void)
-{
-    // Create Mutex
-    xMutex = xSemaphoreCreateMutex();
-
-    // Create tasks
-    xTaskCreate(Task1, "Task1", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-    xTaskCreate(Task2, "Task2", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-
-    // FreeRTOS Scheduler
-    vTaskStartScheduler();
-
-    // if return something wrong
-    while (1)
-    {
+public:
+    Mutex() {
+        //Create
+        mutex = xSemaphoreCreateMutex();
     }
 
-    return 0;
-}
+    ~Mutex() {
+        //Delete
+        vSemaphoreDelete(mutex);
+    }
+
+    void lock() {
+        //lock
+        xSemaphoreTake(mutex, portMAX_DELAY);
+    }
+
+    void unlock() {
+        //unlock
+        xSemaphoreGive(mutex);
+    }
+
+    //
+    bool try_lock() {
+        return xSemaphoreTake(mutex, 0) == pdPASS;
+    }
+};
